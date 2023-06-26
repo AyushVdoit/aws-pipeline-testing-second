ca3ce990f7d6815ff9aaa01783e341770cd2b5f8
AyushVdoit <ayushjaiswal.vdoit@gmail.com>
2023-06-26 15:02:10 +0530

    "first commit"

---
+++ app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+from aws_pipeline_testing_second.aws_pipeline_testing_second_stack import AwsPipelineTestingSecondStack
+
+app = cdk.App()
+AwsPipelineTestingSecondStack(app, "AwsPipelineTestingSecondStack",
+    env=cdk.Environment(account='994546969703', region='us-west-2')
+)
+
+app.synth()
